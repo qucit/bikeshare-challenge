a907d81c6e551c6b96b31297218f59babc9450a4
Chen Long <long@qucit.com>
2014-08-07 12:00:54 +0200

    add the other variables and ameliore for scrip

--- submissions/LONG714/scrip_graphs_regression.py
+++ submissions/LONG714/scrip_graphs_regression.py
@@ -11,7 +11,7 @@ import statsmodels.api as sm
 from __future__ import division
 import math
 from datetime import timedelta
-
+from matplotlib.ticker import FormatStrFormatter
 
 DATA_PATH = '/Users/chenlong/projet/occupation/snapshots-bdx.csv-2.bz2'
 WEATHER_PATH = '/Users/chenlong/projet/occupation/weather/weather.csv'
@@ -37,13 +37,18 @@ def model_1(timeseries, tau):
         predicted.append( timeseries[t-tau] )
     return np.array(predicted)
 
+
 def increment(timeseries,tau):
+    """
+       increment of bikes for a time step tau
+    """
     increment = []
     for i in range(1,tau+1):
         increment.append(model_1(timeseries,i-1)-model_1(timeseries,i))
     return increment
 
 
+""" import data"""
 
 data = pd.read_csv(DATA_PATH, \
 	sep=';',encoding='latin1', parse_dates=["date"],compression='bz2', dayfirst=True, index_col="date")
@@ -58,11 +63,46 @@ data_weather.index = date_time
 data_weather = data_weather.sort_index()
 data_weather_comp = data_weather.asfreq('1h',method = 'pad')
 
+
+
+""" If you want to change the variable, there are 2 parts to change, One is histogramme, another is for creat data_pluie_incr """
+
+
 """histogramme """
-### add .map(lambda x: round(x)) for variables not precipitation_in_mmh
-pluie=data_weather_comp['precipitation_in_mmh'].tolist()
+"""add .map(lambda x: round(x)) for variables not precipitation_in_mmh (before .dropna())"""
+
+pluie=data_weather_comp['precipitation_in_mmh'].dropna()
 unique_pluie=np.unique(pluie)
 plt.hist(pluie,unique_pluie)
+plt.show()
+
+""" an histogramme beautiful and clear"""
+bins_choose = 30 ## il faut bien choisir bins for differents variables, we propose bins=30
+fig, ax = plt.subplots()
+counts, bins, patches = ax.hist(pluie,bins = bins_choose, facecolor='yellow', edgecolor='gray')
+
+ax.set_xticks(bins)
+ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+
+
+twentyfifth, seventyfifth = np.percentile(pluie, [25, 75])
+for patch, rightside, leftside in zip(patches, bins[1:], bins[:-1]):
+    if rightside < twentyfifth:
+        patch.set_facecolor('green')
+    elif leftside > seventyfifth:
+        patch.set_facecolor('red')
+
+
+bin_centers = 0.5 * np.diff(bins) + bins[:-1]
+for count, x in zip(counts, bin_centers):
+    # Label the raw counts
+    ax.annotate(str(count), xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0, -18), textcoords='offset points', va='top', ha='center')
+
+    # Label the percentages
+    percent = '%0.0f%%' % (100 * float(count) / counts.sum())
+    ax.annotate(percent, xy=(x, 0), xycoords=('data', 'axes fraction'),
+        xytext=(0, -32), textcoords='offset points', va='top', ha='center')
 plt.show()
 
 
@@ -88,7 +128,7 @@ for j in range(1,140):
     data_one_station['increment'] = increment(data_one_station['bikes'],1)[0]
     increment_station.append(data_one_station['increment'])  
     """ change precipitation_in_mmh to other variable of weather to look his relation with increment or increment_mean """ 
-    # we add .map(lambda x: round(x)) before tolist() if we use others variables (so we use just values integers)
+    """we add .map(lambda x: round(x)) before tolist() if we use others variables (so we use just values integers)"""
     data_one_station['pluie']=data_weather_comp['precipitation_in_mmh'].tolist()[:len(data_one_station)]
     pluie_station.append(data_one_station['pluie']) 
     id_station.append(data_one_station['station_id'])
@@ -102,6 +142,7 @@ t2 = time.clock()
 print t2-t1
 
 
+""" graphe of increment_mean of all stations """
 data_pluie_incr.groupby('pluie').mean()
 data_pluie_incr.groupby('pluie').mean()['increment'].plot()
 plt.ylabel('Increment_moyen_abs')
@@ -116,7 +157,7 @@ mean_by_station_pluie = data_pluie_incr.groupby(['station_id','pluie']).mean()
 
 
 ### desine graphes with the points reals and fittedvalues from regression
-total_stations = 139
+total_stations = 1
 for i in range(1,total_stations+1):
     data_mean_by_station = mean_by_station_pluie.query('station_id=='+str(i))
     I_mean_by_station=data_mean_by_station['increment']
